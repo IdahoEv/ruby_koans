6c287e957501155a07c7db3c36741bbb83b2a318
Evan Dorn <evan@lrdesign.com>
2011-09-25 12:47:43 -0700

    5

--- about_asserts.rb
+++ about_asserts.rb
@@ -19,7 +19,7 @@ class AboutAsserts < EdgeCase::Koan
   # To understand reality, we must compare our expectations against
   # reality.
   def test_assert_equality
-    expected_value = __
+    expected_value = 2
     actual_value = 1 + 1
 
     assert expected_value == actual_value
@@ -27,7 +27,7 @@ class AboutAsserts < EdgeCase::Koan
 
   # Some ways of asserting equality are better than others.
   def test_a_better_way_of_asserting_equality
-    expected_value = __
+    expected_value = 2
     actual_value = 1 + 1
 
     assert_equal expected_value, actual_value
@@ -35,6 +35,6 @@ class AboutAsserts < EdgeCase::Koan
 
   # Sometimes we will ask you to fill in the values
   def test_fill_in_values
-    assert_equal __, 1 + 1
+    assert_equal 2, 1 + 1
   end
 end
