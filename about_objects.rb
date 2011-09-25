6c1a85d6f702f456361d4e7bc08f24f5d8e24942
Evan Dorn <evan@lrdesign.com>
2011-09-25 13:03:24 -0700

    15

--- about_objects.rb
+++ about_objects.rb
@@ -31,19 +31,21 @@ class AboutObjects < EdgeCase::Koan
   end
 
   def test_some_system_objects_always_have_the_same_id
-    assert_equal __, false.object_id
-    assert_equal __, true.object_id
-    assert_equal __, nil.object_id
+    assert_equal 0, false.object_id
+    assert_equal 2, true.object_id
+    assert_equal 4, nil.object_id
   end
 
   def test_small_integers_have_fixed_ids
-    assert_equal __, 0.object_id
-    assert_equal __, 1.object_id
-    assert_equal __, 2.object_id
-    assert_equal __, 100.object_id
+    assert_equal 1, 0.object_id
+    assert_equal 3, 1.object_id
+    assert_equal 5, 2.object_id
+    assert_equal 201, 100.object_id
 
     # THINK ABOUT IT:
     # What pattern do the object IDs for small integers follow?
+    #
+    # N.id = 2N+1 
   end
 
   def test_clone_creates_a_different_object
